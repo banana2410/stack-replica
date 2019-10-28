ee3b6d33515354e047e6fa703e7e4ac1c8f2b448
banana2410 <56703181+banana2410@users.noreply.github.com>
2019-10-28 20:33:37 +0100

    Second Update
    Setting up the scene, initial scripts for Cube and GameManager

---
+++ Stack Replica/Assets/Cube.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    public static Cube CurrentCube { get; private set; }
+    private void Awake()
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    private void OnEnable()
+    {
+        CurrentCube = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * Time.deltaTime * _speed;
+    }
+    public void Stop()
+    {
+        transform.position = transform.position;
+    }
+}
